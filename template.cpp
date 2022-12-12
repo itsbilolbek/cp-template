2bf55b50d571a323f25c3e88282704806d88712c
itsbilolbek <113835460+itsbilolbek@users.noreply.github.com>
2022-12-13 00:20:50 +0500

    Update template.cpp
    Added more vector function defines and more

--- template.cpp
+++ template.cpp
@@ -1,10 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define ull unsigned long long
+typedef long long ll;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
 #define endl "\n"
-#define rep(i, a, n) for(auto (i) = (a); (i) < (n); ++(i))
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define ar array
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
 
 void solve()
 {
@@ -15,12 +32,13 @@ int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+    cout.tie(0);
     freopen("input.txt", "r", stdin);
     
-    int t;
-    cin >> t;
+    int tc = 1;
+    cin >> tc;
     
-    while (t--)
+    while (tc--)
     {
         solve();
         cout << endl;
