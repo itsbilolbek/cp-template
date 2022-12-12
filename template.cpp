2dc53fe25aa85149eb1b45ef33731304ce389437
Bilolbek <bilolbeknormuminov@gmail.com>
2022-12-12 21:08:42 +0500

    Initial commit

---
+++ template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define endl "\n"
+#define rep(i, a, n) for(auto (i) = (a); (i) < (n); ++(i))
+
+void solve()
+{
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    freopen("input.txt", "r", stdin);
+    
+    int t;
+    cin >> t;
+    
+    while (t--)
+    {
+        solve();
+        cout << endl;
+    }
+
+    return 0;
+}
