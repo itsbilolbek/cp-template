82ce59ccf09ebb48c87fd87a5392a4c1f295bd9b
itsbilolbek <113835460+itsbilolbek@users.noreply.github.com>
2022-12-13 00:35:59 +0500

    Update template.cpp
    More defines

--- template.cpp
+++ template.cpp
@@ -1,27 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef long double ld;
+#define LOCAL
 
-typedef vector<int> vi;
-typedef pair<int, int> pi;
+/* TYPES  */
+#define ar array
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
 
+
+/* FUNCTIONS */
+#define f(i,s,e) for(long long int i=s;i<e;i++)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+
+/* UTILS */
 #define endl "\n"
 #define REP(i, a, b) for(auto i = a; i <= b; ++i)
-
 #define F first
 #define S second
-#define PB push_back
-#define MP make_pair
-
-#define ar array
+#define sp <<" "<<
 #define sza(x) ((int)x.size())
 #define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define fora(a) for(auto &u:a)
+#define printv(a) {for(auto u:a) cout<<u<<" "; cout<<endl;}
 
+/* Constants */
 const ll MOD = 1e9 + 7;
 const ll INF = 1e9;
 const ld EPS = 1e-9;
+
 
 void solve()
 {
@@ -33,7 +50,11 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
+    
+    #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    #endif
     
     int tc = 1;
     cin >> tc;
@@ -46,3 +67,4 @@ int main()
 
     return 0;
 }
+
